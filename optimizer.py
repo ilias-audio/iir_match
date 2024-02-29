0dfff9ced17de9212bda6117ccee7315fb412a58
Ilias Ibnyahya <i.ibnyahya@qmul.ac.uk>
2024-02-29 15:18:26 +0000

    use 3 bands

--- optimizer.py
+++ optimizer.py
@@ -23,14 +23,18 @@ def evaluate_mag_response(
     Q_1: torch.Tensor,
     f_c_2: torch.Tensor,
     G_2: torch.Tensor,
-    Q_2: torch.Tensor
+    Q_2: torch.Tensor,
+    f_c_3: torch.Tensor,
+    G_3: torch.Tensor,
+    Q_3: torch.Tensor
 ):
 
   bell_1 = bell_filter(f, f_c_1, G_1, Q_1)
   bell_2 = bell_filter(f, f_c_2, G_2, Q_2)
+  bell_3 = bell_filter(f, f_c_3, G_3, Q_3)
 
 
-  response = bell_1 * bell_2
+  response = bell_1 * bell_2 * bell_3
 
 
   return response
@@ -119,16 +123,23 @@ target_F2 = (torch.tensor(5000.0))
 target_G2 = (torch.tensor(10.0**(6.0/40)))
 target_Q2 = (torch.tensor(0.5))
 
-target_response = evaluate_mag_response(f, target_F1, target_G1, target_Q1, target_F2, target_G2, target_Q2)
+target_F3 = (torch.tensor(500.0))
+target_G3 = (torch.tensor(10.0**(-2.0/40)))
+target_Q3 = (torch.tensor(0.9))
+
+target_response = evaluate_mag_response(f, target_F1, target_G1, target_Q1, 
+                                        target_F2, target_G2, target_Q2,
+                                        target_F3, target_G3, target_Q3)
 
 print(target_response.max())
 
-n_iters = 5000
+n_iters = 10000
 
 parameters = torch.nn.ParameterList()
 
 parameters.append(torch.tensor([0.1, .9, .0]))
 parameters.append(torch.tensor([0.9, .9, .0]))
+parameters.append(torch.tensor([0.2, .9, .0]))
 
 # F1 = torch.nn.Parameter(torch.tensor(.1))
 # G1 = torch.nn.Parameter(torch.tensor(.9))
@@ -147,7 +158,10 @@ print(((target_F1)),
       ((target_Q1)),
       ((target_F2)),
       ((20*np.log10(target_G2))),
-      ((target_Q2)))
+      ((target_Q2)),
+      ((target_F3)),
+      ((20*np.log10(target_G3))),
+      ((target_Q3)))
 
 pbar = tqdm(range(n_iters))
 for n in pbar:
@@ -163,7 +177,10 @@ for n in pbar:
       q_denormalize(torch.sigmoid(parameters[0][2])),
       frequency_denormalize(torch.sigmoid(parameters[1][0])),
       gain_denormalize(torch.sigmoid(parameters[1][1])),
-      q_denormalize(torch.sigmoid(parameters[1][2]))
+      q_denormalize(torch.sigmoid(parameters[1][2])),
+      frequency_denormalize(torch.sigmoid(parameters[2][0])),
+      gain_denormalize(torch.sigmoid(parameters[2][1])),
+      q_denormalize(torch.sigmoid(parameters[2][2]))
     )
 
 
@@ -180,9 +197,12 @@ print("PREDICTION")
 print(frequency_denormalize(torch.sigmoid(parameters[0][0])),
       gain_denormalize(torch.sigmoid(parameters[0][1])),
       q_denormalize(torch.sigmoid(parameters[0][2])),
-      frequency_denormalize(torch.sigmoid(parameters[0][0])),
-      gain_denormalize(torch.sigmoid(parameters[0][1])),
-      q_denormalize(torch.sigmoid(parameters[0][2])))
+      frequency_denormalize(torch.sigmoid(parameters[1][0])),
+      gain_denormalize(torch.sigmoid(parameters[1][1])),
+      q_denormalize(torch.sigmoid(parameters[1][2])),
+      frequency_denormalize(torch.sigmoid(parameters[2][0])),
+      gain_denormalize(torch.sigmoid(parameters[2][1])),
+      q_denormalize(torch.sigmoid(parameters[2][2])))
 print("ERROR")
 
 
@@ -190,7 +210,7 @@ import matplotlib.pyplot as plt
 
 plt.semilogx(f, 20 * np.log10(pred_response.detach().numpy()), label="Prediction")
 
-plt.plot(np.array([target_F1.detach().numpy(), target_F2.detach().numpy()]), np.array([40*np.log10(target_G1.detach().numpy()) , 40*np.log10(target_G2.detach().numpy())]), 'o')
+plt.plot(np.array([target_F1.detach().numpy(), target_F2.detach().numpy(), target_F3.detach().numpy()]), np.array([40*np.log10(target_G1.detach().numpy()) , 40*np.log10(target_G2.detach().numpy()), 40*np.log10(target_G3.detach().numpy())]), 'o')
 
 
 plt.semilogx(f, 20 * np.log10(target_response.detach().numpy()), label="Target")
