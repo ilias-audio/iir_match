d830a14abd9aa5e9e7247c6a229bdd68e2b3c2fc
Ilias Ibnyahya <ilias@digiconsoles.com>
2025-02-01 16:06:22 +0000

    direct compare with aalto's curve in the paper

--- optimizer.py
+++ optimizer.py
@@ -36,7 +36,7 @@ def frequency_normalize(f):
   max = torch.tensor(MAX_FREQ)
   return log_normalize(f, min, max)
 
-MAX_GAIN_DB =  12.
+MAX_GAIN_DB =  24.
 MIN_GAIN_DB = - MAX_GAIN_DB
 
 def gain_denormalize(g):
@@ -45,8 +45,8 @@ def gain_denormalize(g):
   return lin_denormalize(g, min, max)
 
 def gain_normalize(g):
-  min = torch.tensor(-12.0)
-  max = torch.tensor(12.0)
+  min = torch.tensor(MIN_GAIN_DB)
+  max = torch.tensor(MAX_GAIN_DB)
   return lin_normalize(g, min, max)
 
 MIN_Q = 0.01
@@ -72,7 +72,7 @@ def evaluate_mag_response(
     NUM_OF_BANDS = len(F)
     
     # First band: Low Shelf
-    response = RBJ_HighShelf(x, F[0], G[0], Q[0]).response
+    response = RBJ_LowShelf(x, F[0], G[0], Q[0]).response
 
     # Middle bands: Bell filters
     for i in range(1, NUM_OF_BANDS - 1):
@@ -87,31 +87,47 @@ def evaluate_mag_response(
 # MAIN
 ###############################################################################
 # set the range of frequencies that we evaluate over
-NUM_OF_BANDS = 4
-NUM_OF_ITER = 3000
-NUM_OF_FREQ = 256
+NUM_OF_BANDS = 12
+NUM_OF_ITER = 30000
+# NUM_OF_FREQ = 512
+
+NUM_OF_DELAYS = 6
+SAMPLE_RATE = 48000
+
+# f = torch.logspace(torch.log10(torch.tensor(20.)), torch.log10(torch.tensor(20000)), NUM_OF_FREQ)
 
 
+import scipy.io 
+target_matrix = scipy.io.loadmat("target_mag.mat")
 
-f = torch.logspace(torch.log10(torch.tensor(20.)), torch.log10(torch.tensor(20000)), NUM_OF_FREQ)
+f = torch.tensor(target_matrix.get('w')).squeeze()
+target_response = torch.tensor(target_matrix.get('target_mag')).squeeze()
 
-Frequencies = np.array([])
-GdB = np.array([])
-Qs = np.array([])
-
-
-for i in range(NUM_OF_BANDS):
-  Frequencies = np.append(Frequencies, frequency_denormalize(np.random.uniform()))
-  GdB = np.append(GdB, gain_normalize(np.random.uniform()))
-  Qs = np.append(Qs, q_denormalize(np.random.uniform()))
-
-target_F = (torch.tensor(Frequencies))
-target_G = (torch.tensor(GdB))
-target_Q = (torch.tensor(Qs))
-
-target_response = evaluate_mag_response(f, target_F, target_G, target_Q)
+target_response = torch.pow(10.0, target_response / 20.0)
+# Frequencies = np.array([])
+# GdB = np.array([])
+# Qs = np.array([])
 
 
+# for i in range(NUM_OF_BANDS):
+#   Frequencies = np.append(Frequencies, frequency_denormalize(np.random.uniform()))
+#   GdB = np.append(GdB, gain_normalize(np.random.uniform()))
+#   Qs = np.append(Qs, q_denormalize(np.random.uniform()))
+
+# target_F = (torch.tensor(Frequencies))
+# target_G = (torch.tensor(GdB))
+# target_Q = (torch.tensor(Qs))
+
+# target_response = evaluate_mag_response(f, target_F, target_G, target_Q)
+
+
+# rt60s = np.linspace(1., 0.1, NUM_OF_FREQ)
+# rt60s[NUM_OF_FREQ // 2] = 0.3
+# target_response = torch.pow(
+#   torch.tensor(10.0, dtype=torch.float32),
+#   torch.tensor((-60.0 / (SAMPLE_RATE * rt60s)) * (1500 / 20.0), dtype=torch.float32)
+# )
+# print(target_response)
 
 parameters = torch.nn.ParameterList()
 
@@ -141,13 +157,13 @@ for n in pbar:
 
 
   # measure the MSE
-  loss = torch.nn.functional.l1_loss((pred_response), (target_response))
+  loss = torch.nn.functional.mse_loss((pred_response), (target_response))
   loss.backward()
   # Scale frequency gradients to have more impact
   with torch.no_grad():
-    parameters[0].grad *= 6  # Boost frequency gradients
-    parameters[1].grad *= 3   # Moderate Q gradients
-    parameters[2].grad *= 1   # Normal gain gradients
+    parameters[0].grad *= 1  # Boost frequency gradients
+    parameters[1].grad *= 1  # Moderate Q gradients
+    parameters[2].grad *= 1  # Normal gain gradients
 
   optimizer.step()
   scheduler.step()
@@ -166,7 +182,7 @@ plt.plot(
 )
 
 plt.semilogx(f, 20 * np.log10(target_response.detach().numpy()), label="Target", linestyle='dotted', color='b')
-plt.plot(target_F.detach().numpy(), (target_G.detach().numpy()), 'o', color='b')
+# plt.plot(target_F.detach().numpy(), (target_G.detach().numpy()), 'o', color='b')
 
 
 
