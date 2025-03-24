e9220b4eef77412ddda4cb720b52d9694a8a085c
Ilias Ibnyahya <35698838+ilias-audio@users.noreply.github.com>
2025-03-24 14:41:57 +0000

    remove comments

--- optimizer.py
+++ optimizer.py
@@ -62,13 +62,7 @@ def sos_mag_response(F: torch.Tensor,     # Center frequencies (NUM_OF_DELAYS x 
 
 
 def convert_proto_gain_to_delay(gamma, delays, fs):
-  # print(delays / fs)
-  # print(gamma.shape)
-  # print(gamma)
-  # print(delays.shape)
-
   gain = gamma * (delays / fs)
-  # print(gain)
   return gain
   
 
@@ -105,19 +99,13 @@ parameters.append(torch.rand(NUM_OF_BANDS,requires_grad=True, device=device, dty
 parameters.append(torch.rand(NUM_OF_BANDS, requires_grad=True, device=device, dtype=torch.float32)) # Common gains
 parameters.append(torch.rand(NUM_OF_BANDS, requires_grad=True, device=device, dtype=torch.float32)) # Common Q
 
-# parameters.append([
-#   torch.nn.Parameter(torch.rand((1), requires_grad=True, device=device, dtype=torch.float32))
-#   for _ in range(NUM_OF_DELAYS*NUM_OF_BANDS)
-# ])
+
 
 optimizer = torch.optim.Adam(parameters, lr=0.1)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, NUM_OF_ITER)
 pbar = tqdm(range(NUM_OF_ITER))
 
 for n in pbar:  
-  # freq_params = torch.stack([param[0] for param in parameters]).view(NUM_OF_DELAYS, NUM_OF_BANDS)
-  # gain_params = torch.stack([param[1] for param in parameters]).view(NUM_OF_DELAYS, NUM_OF_BANDS)
-  # q_params    = torch.stack([param[2] for param in parameters]).view(NUM_OF_DELAYS, NUM_OF_BANDS)
 
   freq_params = torch.sigmoid(parameters[0].unsqueeze(0).repeat(NUM_OF_DELAYS, 1))
   gain_params = torch.sigmoid(parameters[1])
@@ -141,34 +129,17 @@ for n in pbar:
   )
 
   if n % 1000 == 0:
-    # print("Freqs: ", frequency_denormalize(torch.sigmoid(freq_params)).tolist())
-    # print("Gain: " , gain_denormalize(torch.sigmoid(gain_params)).tolist())
-    # print("Q: "    , q_denormalize(torch.sigmoid(q_params)).tolist())
-    # print("Freqs: ", freq_params.tolist())
-    # print("Gain: " , gain_params.tolist())
-    # print("Q: "    , q_params.tolist())
-
-    
+        
     plt.clf()
     plt.semilogx(f.cpu(), 20 * np.log10(pred_responses.detach().cpu().numpy().T), label="Prediction")
     plt.semilogx(f.cpu(), 20 * np.log10(target_responses.detach().cpu().numpy().T), label="Target", linestyle='dotted')
-    # plt.semilogx(sos_freq, 20 * np.log10(sos_mag), label="sos", linestyle='dotted')
-    # plt.plot(frequency_denormalize(torch.sigmoid(freq_params)).detach().cpu(), gain_denormalize(torch.sigmoid(gain_params)).detach().cpu().numpy(), 'o')
     plt.legend()
-    # plt.title("Frequency response matching using stochastic gradient descent")
-    
-    # Define custom tick positions and labels
-    # tick_positions = [1, 10, 30, 100, 300, 1000, 3000, 10000, 20000]  # Explicit positions in Hz
-    # tick_labels = ['1 Hz', '10 Hz', '30 Hz', '100 Hz', '300 Hz', '1 kHz', '3 kHz', '10 kHz', '20 kHz']  # Custom labels
-
-    # plt.xticks(tick_positions, tick_labels)
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Magnitude (dB)")
     plt.savefig("./figures/match"+str(n)+".png")
 
   loss = torch.zeros(1, device=device)
   optimizer.zero_grad()
-  # for i in range(NUM_OF_DELAYS):
   loss = torch.nn.functional.mse_loss(20 * torch.log10((pred_responses)), 20 * torch.log10(target_responses))
   loss.backward(retain_graph=False)
   optimizer.step()
